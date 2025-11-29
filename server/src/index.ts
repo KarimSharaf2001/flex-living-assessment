2d027d75e7d0c2871aa4350ce50ccc45332db038
KarimSharaf2001 <sharafkarim61@gmail.com>
2025-11-30 00:21:57 +0200

    ok900yk

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://flex-reviews.vercel.app", // Update this to match your actual Vercel URL
+      "https://flex-reviews-umber.vercel.app", // Update this to match your actual Vercel URL
     ],
   })
 );
